4dba67d7810d3d75fa56641f0a4a13ddafa621dd
Andy <att35663@uga.edu>
2019-04-04 11:57:18 -0400

    Added screenshot

--- testProject/screens/HomeScreen.js
+++ testProject/screens/HomeScreen.js
@@ -51,6 +51,8 @@ export default class App extends Component {
               placeholder="Enter N value"
               onChangeText={inputtedValue => {
                 this.updateNValue(inputtedValue);
+              }}
+              onEndEditing={value => {
                 this.parseSelectedGrade("10-10-10");
               }}
             />
